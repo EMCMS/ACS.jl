27b8e998b23d23932e76dc850cc4aed96d1ff3fb
Saer Samanipour <s.samanipour@uva.nl>
2022-07-04 17:29:24 +0200

    The first commit of the documenter

---
+++ docs/src/index.md
@@ -0,0 +1,3 @@
+# Advanced Statistics and Chemometrics (ACS)
+
+*A documentation built for the ACS course to help the students during the lectures and for exame preparation* 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using ACS
 using Test
+#using ScikitLearn
+@sk_import decomposition: PCA
 
 @testset "ACS.jl" begin
     # Write your tests here.
+    #@sk_import decomposition: PCA
+    iris = dataset("datasets", "iris")
+    println(iris)
+
+    X = Matrix(iris[:,1:3])
+    s = iris[!,"Species"]
+    #pca = sk_pca(n_components=2)
+    pca = PCA(n_components=2)
+    pca.fit(X)
+
+    #LogisticRegression
+
 end
