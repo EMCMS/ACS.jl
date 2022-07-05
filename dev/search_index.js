c6a9fd167ea0a39aee5cd864e15f253da3eb405d
Documenter.jl <documenter@juliadocs.github.io>
2022-07-05 16:24:18 +0000

    build based on ff4dd0a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Advanced-Statistics-and-Chemometrics-(ACS)","page":"Home","title":"Advanced Statistics and Chemometrics (ACS)","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A documentation built for the ACS course to help the students during the lectures and for exame preparation ","category":"page"}]
+}
