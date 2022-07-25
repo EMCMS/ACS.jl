f3bd436c844583e869d1b9533ba7277a21ea2cb4
Saer Samanipour <s.samanipour@uva.nl>
2022-07-25 15:32:01 +0200

    small bug fix

--- src/ACS.jl
+++ src/ACS.jl
@@ -23,7 +23,7 @@ using HDF5
 using JLD
 
 export dataset, plot, plot!, scatter, scatter!, xlabel!, ylabel!, title!, @sk_import, eigvals, eigvecs, transpose, svd, diagm,
-pinv, diag, xlims!, ylims!, describe, mean, median, bar, bar!, 
+pinv, diag, xlims!, ylims!, describe, mean, median, bar, bar! 
 # Write your package code here.
 
 
