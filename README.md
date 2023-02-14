55ae7a4dac18a7fa2e7fc1dde6f7132fcffc711c
Saer Samanipour <s.samanipour@uva.nl>
2023-02-14 14:00:20 +0100

    The first version is almost done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ACS
 
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/saersamani/ACS.jl?svg=true)](https://ci.appveyor.com/project/saersamani/ACS-jl)
+
+All necessary information about this package can be found in its [documentation](https://emcms.github.io/ACS.jl/dev/).
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,16 @@ using Test
     # Write your tests here.
     #@sk_import decomposition: PCA
     iris = dataset("datasets", "iris")
-    println(iris)
+    #println(iris)
 
     X = Matrix(iris[:,1:3])
     s = iris[!,"Species"]
     #pca = sk_pca(n_components=2)
     pca = PCA(n_components=2)
     pca.fit(X)
+    #println(pca.explained_variance_ratio_)
+
+    @test pca.explained_variance_ratio_[1] > 0.9
 
     #LogisticRegression
 
